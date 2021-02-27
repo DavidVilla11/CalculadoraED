a765317324ed2ccae2ceb00659e73718264d8ca1
David Matias <davidmatiasmots@gmail.com>
2021-02-27 21:49:43 +0100

    Clase Principal para hacer pruebas

---
+++ Principal.java
@@ -0,0 +1,16 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Resta r1 = new Resta ();
+		
+		System.out.println(r1.restaDosEnteros1(4, 2));
+		
+	//Clase Principal para hacer pruebas
+		
+		
+
+	}
+
+}
