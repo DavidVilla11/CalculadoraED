43e62805d5625f2bc9eebd283b6e1aa74a4c4c9a
DavidVilla11 <DavidVilla11@github.com>
2021-03-02 16:34:08 +0100

    Arreglo de fallo clase Producto y prueba de la clase.

--- Principal.java
+++ Principal.java
@@ -3,14 +3,18 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		//Clase Principal para hacer pruebas
 		Resta r1 = new Resta ();
 		
 		System.out.println(r1.restaDosEnteros1(4, 2));
 		
-	//Clase Principal para hacer pruebas
+		//Prueba clase producto
+		Producto p1 = new Producto();
 		
-		
-
+		System.out.println(p1.ProductoNumerosEnteros(4, 6));
+		System.out.println(p1.ProductoNumerosReales(6.4, 2.5));
+		System.out.println(p1.ProductoTresNumerosReales(4.2, 2, 0.5));
+		System.out.println(p1.potencia(5, 7));
 	}
 
 }
