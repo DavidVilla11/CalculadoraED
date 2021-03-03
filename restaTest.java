37002034ba98e2b9f08cc15de51c44ce07f25446
David Matias <davidmatiasmots@gmail.com>
2021-03-03 21:53:01 +0100

    Creación de más pruebas

--- restaTest.java
+++ restaTest.java
@@ -4,17 +4,26 @@ import org.junit.jupiter.api.Test;
 
 class restaTest {
 
-	@Test
+@Test
 	void test() {
 		Resta test =  new Resta();
 		int resultado = test.restaDosEnteros1(6, 3);
 		assertEquals(3, resultado);
+	
 		
+@Test 
+		//En este test probamos que el método devuelva NaN en caso de 
+		//introducir 0 en los dos parámetros.
+		void RestaReales() {
+			Resta test = new Resta();
+			double resultado = test.restaDosReales(33, 3);
+			assertEquals(30,resultado);
+		}	
 		
 		
 	}
 	
-	@Test 
+@Test 
 	void RestaDeTresEnteros () {
 		
 	 Resta test = new Resta();
@@ -23,14 +32,14 @@ class restaTest {
 	
 	}
 	
-	@Test 
+@Test 
 	void RestaReales() {
 		Resta test = new Resta();
 		double resultado = test.restaDosReales(33, 3);
 		assertEquals(30,resultado);
 	}
 	
-	@Test
+@Test
 	
 	void RestaAcumulado() {
 		Resta test = new Resta();
@@ -38,6 +47,17 @@ class restaTest {
 		assertEquals (5,resultado,"El resultado obtenido debe de ser 5 ya que el valor acumulado era 10");
 	}
 	
+@Test
+	
+	//Si los dos parámetros son 0 el resultado será NaN
+	//He tenido que realizar algún cambio en el código del método
+	// de restaDeDosEnteros para que funcinase este Test
+	void testNaN() {
+		Resta test = new Resta();
+		float resultado = test.restaDosEnteros1(0, 0);
+		double esperado = Double.NaN;
+		assertEquals(esperado, resultado);
+	}
 	
 	
 	
